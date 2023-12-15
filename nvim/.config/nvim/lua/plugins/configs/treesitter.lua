3d496c77c9c0676063602c1cb05438cfabca7896
Kamron Mirsamatov <kronusk2@gmail.com>
2023-12-15 15:55:39 +0100

    setup for rust

--- nvim/.config/nvim/lua/plugins/configs/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,8 +1,12 @@
 local options = {
-    ensure_installed = { "rust" , "python", "go", "typescript", "javascript", "lua", "markdown" },
-    highlight = { enable = true },
-    additional_vim_regex_highlighting = false,
-    indent = { enable = true }
+  ensure_installed = { "lua" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
 }
 
 return options
