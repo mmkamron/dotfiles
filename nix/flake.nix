82e93f508c417af75c8a887fb776ce33e161e7ac
Kamron  Mirsamatov <kamronmirsamatov@MacBook-102.local>
2024-11-22 21:16:47 +0500

    alacritty in spotlight

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,17 +9,38 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.neovim
+        [ 
 	  pkgs.tmux
 	  pkgs.git
+	  pkgs.neovim
 	  pkgs.fastfetch
 	  pkgs.stow
+	  pkgs.alacritty
+	  pkgs.mkalias
         ];
-
+      system.activationScripts.applications.text = let
+	env = pkgs.buildEnv {
+	  name = "system-applications";
+	  paths = config.environment.systemPackages;
+	  pathsToLink = "/Applications";
+	};
+      in
+	pkgs.lib.mkForce ''
+	# Set up applications.
+	echo "setting up /Applications..." >&2
+	rm -rf /Applications/Nix\ Apps
+	mkdir -p /Applications/Nix\ Apps
+	find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+	while read -r src; do
+	  app_name=$(basename "$src")
+	  echo "copying $src" >&2
+	  ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+	done
+      '';
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
