cbd48229addaf09da74ac555de78454045b31ac1
Kamron Mirsamatov <mmkamron@Kamron.local>
2025-07-29 08:48:36 +0500

    update

---
+++ nix/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+    let
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = with pkgs;
+          [
+            neovim
+            git
+            eza
+            zoxide
+          ];
+
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+    {
+      darwinConfigurations."Kamron" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Kamron".pkgs;
+    };
+}
