97b94f10909fef132a0a482982106a6acb718e0c
Kamron Mirsamatov <kronusk2@gmail.com>
2025-08-02 10:26:41 +0500

    minimal nixos

--- nix/modules/nixos/disk-config.nix
+++ nix/modules/nixos/disk-config.nix
@@ -1,21 +1,24 @@
 _: {
-  # This formats the disk with the ext4 filesystem
-  # Other examples found here: https://github.com/nix-community/disko/tree/master/example
   disko.devices = {
     disk = {
-      vdb = {
-        device = "/dev/%DISK%";
+      main = {
         type = "disk";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
+              size = "1G";
               type = "EF00";
-              size = "100M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
